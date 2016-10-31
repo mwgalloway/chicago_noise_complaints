08cee8a8d46265fe43f6553a6f08eb4d4830ef3e
Matthew Galloway <email": null>
2016-10-30 19:55:46 -0500

    Remove geographic true from migrations to allow within? method

--- db/migrate/20161031000710_create_complaints.rb
+++ db/migrate/20161031000710_create_complaints.rb
@@ -1,7 +1,7 @@
 class CreateComplaints < ActiveRecord::Migration[5.0]
   def change
     create_table :complaints do |t|
-      t.st_point :latlon, :geographic => true
+      t.st_point :latlon
       # t.integer  :api_id
       t.integer  :neighborhood_id
 
